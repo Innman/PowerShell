ab4816693a670275925cf8dbfe0a21616e0fd1fe
Nick Faul <nick.g.faul@gmail.com>
2018-10-29 10:08:59 -0500

    Update compare-psversion.ps1

--- compare-psversion.ps1
+++ compare-psversion.ps1
@@ -1,4 +1,17 @@
 
+
+<#
+
+.SYNOPSIS
+This command will compare the current PSversion and return True if the current version is greater than or equal to 5.0.0.0
+
+.DESCRIPTION
+This command will compare the current PSversion and return True if the current version is greater than or equal to 5.0.0.0
+
+.EXAMPLE
+./compare-psversion.ps1
+
+#>
 #returns True is PSversion is greater than or equal to 5.0.0.0
 
 $PSVersionTable.PSVersion -ge [Version]'5.0.0.0'
