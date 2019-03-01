63154350c7fdbf3e853361ce071cf1b6f30e4abe
Nick Faul <nick.g.faul@gmail.com>
2019-02-28 18:27:30 -0600

    Create Rename-User.psm1

---
+++ Rename-User.psm1
@@ -0,0 +1,28 @@
+<#.SYNOPSIS
+This cmdlet will change the name of a user in AD. 
+
+.DESCRIPTION
+This cmdlet will change the name of and email of a user in AD.
+.EXAMPLE
+./Rename-User.ps1
+#>
+
+
+
+function Rename-User {
+    [cmdletbinding()]
+    param (
+    [Alias('Name','Identity')]
+    [string] $Username,
+
+    [string] $Newname,
+
+    [Parameter (Mandatory=$true)]
+    [ValidatePattern("^\d{6}$")]
+    [Alias('StudentID','Id')]
+    [string] $EmployeeID
+    ) # end param
+
+    Write-Host "Doing Stuff"
+    Write-Verbose "Doing verbose stuff with $Username"
+}
