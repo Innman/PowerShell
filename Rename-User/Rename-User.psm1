d94becc060b2bfdf9764f96d7ebee594c43ecf06
Nick Faul <nick.g.faul@gmail.com>
2019-03-21 11:35:11 -0500

    Update Rename-User.psm1
    Added some checks to parameters.

--- Rename-User/Rename-User.psm1
+++ Rename-User/Rename-User.psm1
@@ -31,13 +31,36 @@ function Rename-User {
     [string] $EmployeeID
     ) # end param
 
-    Write-Verbose "Verifying Account for $OldLastName, $OldFirstName.."
+    #Checking to see if account name exist in AD
+    $identity = "$OldFirstName$OldLastName"
+
+    Write-Verbose "Verifying Account for $identity..."
+    try{
+        $user = Get-ADUser -Identity $identity -Properties * 
+        if ($user){
+            Write-Verbose "User: $identity found in Active Directory."
+        }
+    }
+    catch{
+        Write-Verbose "User: $identity was not found Active Directory."
+        Write-Error "User: $identity was not found Active Directory. Please check input parameters and try again."
+        exit
+    }
+    
     Write-Verbose "Checking user account ID..."
-    Write-verbose "ID: $EmployeeID matches to account $OldFirstName$OldLastName."
+    if ($user.EmployeeID -eq  $EmployeeID){
+        Write-Verbose "ID: $EmployeeID matches to account $OldFirstName$OldLastName."
+    }
+    else{
+        Write-Verbose "ID: $EmployeeID does not match to account $OldFirstName$OldLastName."
+        Write-Error "ID: $EmployeeID does not match to account $OldFirstName$OldLastName. Please check input parameters and try again."
+        exit
+    }
 
     <#Find AD user with $EmployeeID and check to see if sam.accountname = $username
         If both are match the selected user from AD, change the name
         Format = Last, First
 
     #>
+    
 }
