1bd59496efb49ab2b384734c14698610b0fbf3e6
Nick Faul <nick.g.faul@gmail.com>
2019-10-02 09:32:17 -0500

    Removed some info

--- EmailForwarding.ps1
+++ EmailForwarding.ps1
@@ -1,24 +1,30 @@
 #This script must be ran in a powershell prompt with the ActiveDirectory module installed ("get-module ActiveDirectory" should return output if installed)
-#This script should run with elevated permissions that allows for writing to editing user in AD
+#This script should run with elevated permissions that allows for writing to editing user in AD (inside a -admin elevated PS prompt)
 #When prompted for credentials you should use an account that has permissions to edit 0365 mailboxes.
 
 $UserCredential = Get-Credential
 Connect-MsolService -Credential $UserCredential
-$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri "<ConnectionURI>" -Credential $UserCredential -Authentication Basic -AllowRedirection
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $UserCredential -Authentication Basic -AllowRedirection
 Import-PSSession $Session
 
+
+$Logfile = "C:\Path\To\Logfile.txt"
 Function LogWrite
 {
    Param ([string]$logstring)
-
    Add-content $Logfile -value $logstring
 }
 
 $users = Import-Csv -Path .\EmailForwarding.csv -Header 'Name','ExternalAddress'
 foreach ($user in $users) {
-    Get-ADUser -Identity $user.name | Move-ADObject -TargetPath "<Target OU>"
-    Add-ADGroupMember -Identity "<GROUP NAME TO ADD>" -Members $user.name
+    Get-ADUser -Identity $user.name | Move-ADObject -TargetPath "TargetOU"
+    LogWrite "Finding $($user.name)"
+    LogWrite "Adding user to group GroupToAdd"
+    Add-ADGroupMember -Identity "GroupToAdd" -Members $user.name
+    LogWrite "Adding $($user.name) to GroupToAdd"
     Set-Mailbox $user.name -ForwardingSmtpAddress $user.ExternalAddress -DeliverToMailboxAndForward $true -Verbose
+    LogWrite "Setting forwarding address on $($user.name) to $($user.ExternalAddress)"
+    #get-Mailbox $user.name | select Name, ForwardingSMTPAddress, DeliverToMailboxAndForward
 }
 
 #check to verify forwarding address is set
